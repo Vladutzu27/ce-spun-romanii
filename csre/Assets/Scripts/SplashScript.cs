4a451f7cad10eb0e5a663144c5d779098e4e1610
Vlad-Andrei Medințu <80452570+Vladutzu27@users.noreply.github.com>
2024-10-10 12:01:03 +0300

    Improved the menu

---
+++ csre/Assets/Scripts/SplashScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class SplashScript : MonoBehaviour
+{
+    [SerializeField]
+
+    private string[] splashes;
+    public TextMeshProUGUI splash;
+
+    void Awake()
+    {
+        splashes = new string[]
+            {
+                "DEFINITIVE EDITION",
+                "AS SEEN ON DOIZERO",
+                "AS SEEN ON R/PROGRAMARE",
+                "AS SEEN ON YOUTUBE",
+                "COMPLET NEOFICIAL",
+                "CAȘCABRAL",
+                "CU CAAAABRAAALLL",
+                "AM ÎNTREBAT O SUTĂ DE ROMÂNI",
+                "COMPLET OPEN SOURCE",
+            };
+    }
+
+    void Start()
+    {
+        splash.text = GetSplash();
+    }
+
+    public string GetSplash()
+    {
+        return splashes[Random.Range(0, splashes.Length)];
+    }
+}
--- csre/Assets/Scripts/VideoScript.cs
+++ csre/Assets/Scripts/VideoScript.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoScript : MonoBehaviour
 {
+    public GameObject cone;
+    public Video.VideoClip icsuri;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        var videoPlayer = cone.AddComponent<UnityEngine.Video.VideoPlayer>();
     }
 
     // Update is called once per frame
